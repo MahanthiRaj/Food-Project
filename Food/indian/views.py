cdc81428cddab3728d1c6915a381f19441734281
Mahanthi Raju <mahanthi0505@gmail.com>
2025-05-28 20:04:14 -0400

    Food project

--- Food/indian/views.py
+++ Food/indian/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
-
+from django.shortcuts import render
 
 # Create your views here.
